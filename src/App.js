2f76c628c3c5fbd7c73aa437d84ff495fc68ae47
Synapsis CI <webmaster@synapsis.com.ar>
2021-05-26 13:48:36 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
+import valorant from './valorant.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" alt="Valorant Gamers" />
+        <img src={valorant} className="App-valorant" alt="Valorant Gamers" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://playvalorant.com/es-mx/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <span className="bot">GAMERS</span>
         </a>
       </header>
     </div>
